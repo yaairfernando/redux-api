e36116d668268ff4ea38a511ab4e236f07127229
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-01-11 12:09:05 -0600

    Creating types file and initial reducer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(reducers, initialState, applyMiddleware(...middleware));
 
 ReactDOM.render(
   <Provider store={store}>
