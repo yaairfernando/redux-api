5bab7c82691040272bb94bfd45b0ae719e306e55
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-01-11 10:12:39 -0600

    Creating App component and installing axios to make the api request

---
+++ src/components/Posts.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  
+  componentDidMount() {
+
+  }
+  render() {
+    return(
+      <div>Posts</div>
+    )
+  };
+};
+
+export default Post;
